932ee6c3aede213cbe4da49177b6161aaf74fd26
Grayson Cunha <grayson.silva@hotmail.com>
2019-02-07 10:54:05 -0200

    React connect with Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import List from "./components/List";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="row mt-5">
+        <div className="col-md-4 offset-md-1">
+          <h2>Articles</h2>
+          <List />
+        </div>
       </div>
     );
   }
